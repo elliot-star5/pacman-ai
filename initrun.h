a54f55dc1a2f37ec7ce2170a81e1b950981bfe1c
elliot-star <matticatee@protonmail.com>
2022-02-17 21:26:46 +0530

    renamed main.c to initrun.h

--- initrun.h
+++ initrun.h
@@ -1,3 +1,6 @@
+#ifndef __INIT_RUN_H
+#define __INIT_RUN_H 1
+
 // Using standard library, standard bool, standard IO, SDL, SDL_image..
 #include <stdio.h>
 #include <string.h>
@@ -16,24 +19,6 @@
 #include "include/LTexture.h"       /* texture loading */
 #include "include/LTimer.h"         /* timer */
 
-
-// Screen constants
-const int SCREEN_WIDTH = 1024;
-const int SCREEN_HEIGHT = 640;
-const int SCREEN_FPS = 60;
-
-
-// current window instance
-struct LWindow gWindow = _LWindow;
-
-// font texture 
-TTF_Font *gFont = NULL;
-
-// renderer for rendering texture
-SDL_Renderer* gRenderer = NULL;
-
-// texture from image 
-struct LTexture gSceneTexture = _LTexture;
 
 
 // initialize SDL 
@@ -202,3 +187,4 @@ void runMainLoop()
   }
 }
 
+#endif
